71fa0b69c8c9c4ee09a16f0e817e5829331bcbcc
Luenci <sheng.lu@yo-star.com>
2021-11-25 16:19:03 +0800

    refactor: 重构 Render & gin 框架定制化

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/luenci/gopkg
 
 go 1.16
 
-require github.com/json-iterator/go v1.1.12
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/json-iterator/go v1.1.12
+	github.com/luenci/errors v0.0.0-20211125073905-37777a2e5521
+)
--- render.go
+++ render.go
@@ -1,7 +1,11 @@
 package gopkg
 
 import (
+	"encoding/json"
 	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/luenci/errors"
 )
 
 // Result Request the result structure.
@@ -24,15 +28,34 @@ var resultPool = &sync.Pool{
 }
 
 // Response response method.
-func Response(data *Result)*Result{
+func Response(ctx *gin.Context, code int, data interface{}) {
+	// 状态码: 根据code（服务码）规则生成 eg: Code:200001, httpStatus: 200
+	httpStatus := int(code / 1000)
 	result := resultPool.Get().(*Result)
 	defer func() {
 		result.reset()
 		resultPool.Put(result)
 	}()
-	return &Result{
-		Data: data.Data,
-		Msg:  data.Msg,
-		Code: data.Code,
+
+	var err error
+	if _, ok := data.(error); ok {
+		err = data.(error)
 	}
+	if err != nil {
+		coder := errors.ParseCoder(err)
+		result.Code = coder.Code()
+		if coder.String() != "" {
+			result.Msg = coder.String()
+		} else {
+			result.Msg = err.Error()
+		}
+	} else {
+		result.Code = code
+		result.Msg = "Success"
+	}
+
+	result.Data = data
+	b, _ := json.Marshal(&result)
+	ctx.Set("ResponseBody", b)
+	ctx.JSON(httpStatus, result)
 }
